c6c529e84e3037cf3330cf36e37e4b5ba2aa4b0c
hp <hp@hp-pc>
2023-05-29 21:30:25 +0530

    Login Page Test Cases

---
+++ src/main/java/PageLayer/LoginPage.java
@@ -0,0 +1,9 @@
+package PageLayer;
+
+public class LoginPage {
+	
+	public void LoginFunctionality() {
+		System.out.println("LoginPage Class");
+	}
+
+}
---
+++ src/test/java/Tutorail1/LoginPageTest.java
@@ -0,0 +1,14 @@
+package Tutorail1;
+
+import PageLayer.LoginPage;
+
+public class LoginPageTest {
+
+	public static void main(String[] args) {
+		
+		LoginPage login=new LoginPage();
+		login.LoginFunctionality();
+
+	}
+
+}
